bffa79e1cd310b2e24d7c74f29f762bcb73f911d
Vlad <darthpikachu2956@gmail.com>
2024-08-03 23:54:46 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+strings_positions = {}
+str_num = 0
+
+
+def custom_write(file_name, strings):
+    for i in strings:
+        file = open(file_name, 'a', encoding='utf-8')
+        global str_num
+        str_num += 1
+        strings_positions.update({(str_num, file.tell()): i})
+        file.write(f'{i}\n')
+        file.close()
+    return strings_positions
+
+
+spisok = ['Text for tell.', '1234', 'Камчатка', 'bye']
+result = custom_write('example2.txt', spisok)
+
+for elem in result.items():
+    print(elem)
